066ae50593f2c2d14f89bce170e9c38fa441e2b6
TIP1 <tip219@bk.ru>
2020-07-19 14:23:04 +0300

    simple counter

--- src/Ccomponent.js
+++ src/Ccomponent.js
@@ -6,34 +6,40 @@ export default class Ccomponent extends Component {
         super(props)
     
         this.state = {
-             visibility: false
+             count: 0
         };
 
-        this.handleClick = this.handleClick.bind(this);
+        this.increment = this.increment.bind(this);
+        this.decrement = this.decrement.bind(this);
+        this.reset = this.reset.bind(this);
     }
-    handleClick() {
+    increment() {
         this.setState(state => ({
-            visibility: !state.visibility
-        }));
+            count: state.count + 1
+            }));
+    }
+
+    decrement() {
+        this.setState(state => ({
+            count: state.count - 1
+            }));
+    }
+
+    reset() {
+        this.setState({
+            count: 0
+        });
     }
     
     render() {
-            if (this.state.visibility) {
-                return (
-                    <div>
-                        <h1>Now you see me</h1>
-                        <button onClick={this.handleClick} >Click</button>
-                    </div>
-                );
-            }
-            else {
-                return (
-                    <div>                    
-                        <button onClick={this.handleClick} >Click</button>
-                    </div>
-                )
-            }
+            return(
+                <div>
+                    <button onClick={this.increment}>increment</button>
+                    <button onClick={this.decrement}>decrement</button>
+                    <button onClick={this.reset}>reset</button>
+                    <h1>Current: {this.state.count}</h1>
+                </div>
+            )
     }
 }
 
-Ccomponent.defaultProps = { name: 'Ignaty'}
