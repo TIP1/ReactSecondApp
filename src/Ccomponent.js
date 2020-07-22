c02a60bbbe09482d64b8faed94122679444ff399
TIP1 <tip219@bk.ru>
2020-07-22 12:53:55 +0300

    router

--- src/Ccomponent.js
+++ src/Ccomponent.js
@@ -1,55 +1,51 @@
 import React, { Component } from 'react'
 import Fcomponent from './Fcomponent'
 import './newStyle.css';
+import Home from './Home'
+import About from './About'
+import Users from './Users'
+import { Switch, Route, Link, BrowserRouter as Router } from 'react-router-dom';
 
 export default class Ccomponent extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            error: null,
-            isLoaded: false,
-            items: []
+            
         };
 
     }
     
-    componentDidMount() {
-        fetch("https://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail")
-        .then(res => res.json())
-        .then(
-            (result) => {
-                this.setState({
-                    isLoaded: true,
-                    items: result.drinks
-                });
-            },
-            (error) => {
-                this.setState({
-                    isLoaded: true,
-                    error
-                });
-            }
-        )
-    }
-
     render () {
-        const {error, isLoaded, items} = this.state;
-        if (error) {
-            return <p>Error {error.message}</p>
-        } else if (!isLoaded) {
-            return <p>Loading...</p>
-        } else {
-            return(
-                <ul>
-                    {items.map(item => (
-                        <li key={item.name}>
-                            {item.strDrink}
-                            <img width="50px" src={item.strDrinkThumb}></img>
-                        </li>
-                    ))}
-                </ul>
-            )
-        }
+        return(
+            <Router>
+                <div>
+                    <nav>
+                        <ul>
+                            <li>
+                                <Link to="/">Home</Link>
+                            </li>
+                            <li>
+                                <Link to="/about">About</Link>
+                            </li>
+                            <li>
+                                <Link to="/users">Users</Link>
+                            </li>
+                        </ul>
+                    </nav>
+                    <Switch>
+                        <Route path="/About">
+                            <About />
+                        </Route>
+                        <Route path="/Users">
+                            <Users />
+                        </Route>
+                        <Route path="/">
+                            <Home />
+                        </Route>
+                    </Switch>
+                </div>
+            </Router>
+        );
     }
 
 }
